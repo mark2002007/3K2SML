40ef1b67c0df2430ff9e8e9234bcfdd3b1244171
Маркіян Мандзак <markmandzak2002@gmail.com>
2022-02-16 13:29:46 +0200

    Homework 1.0
    -Implemented Naive Matrix Multiplication
    -Compared "Naive" versus numpy's matrix multiplication

---
+++ Homeworks/1/task.py
@@ -0,0 +1,25 @@
+import timeit
+import numpy as np
+
+print(timeit.timeit("""
+import numpy as np
+def NaiveMult(A, B, N):
+    C = [[0 for j in range(N)] for i in range(N)]
+    for i in range(N):
+        for j in range(N):
+            for k in range(N):
+                C[i][j] += A[i, k] + B[k, j]
+    return C
+N = 100
+A = np.array([[i + j for j in range(N)] for i in range(N)])
+B = np.array([[i + j for j in range(N)] for i in range(N)])
+NaiveMult(A, B, N)
+""", number = 1))
+
+print(timeit.timeit("""
+import numpy as np
+N = 100
+A = np.array([[i + j for j in range(N)] for i in range(N)])
+B = np.array([[i + j for j in range(N)] for i in range(N)])
+A*B
+""", number = 1))
