5ea3acd6b4ddc3c00b0457a2cd85639019b88d1d
Маркіян Мандзак <markmandzak2002@gmail.com>
2022-04-26 17:55:44 +0300

    Task 1.2 : Multiplication Fix

--- Homeworks/1/task.py
+++ Homeworks/1/task.py
@@ -4,11 +4,12 @@ import numpy as np
 print(timeit.timeit("""
 import numpy as np
 def NaiveMult(A, B, N):
-    C = [[0 for j in range(N)] for i in range(N)]
-    for i in range(N):
-        for j in range(N):
-            for k in range(N):
-                C[i][j] += A[i, k] + B[k, j]
+    m, n = A.shape[0], B.shape[1]
+    C = [[0 for j in range(m)] for i in range(n)]
+    for i in range(m):
+        for j in range(n):
+            for k in range(B.shape[0]):
+                C[i][j] += A[i][k] * B[k][j]
     return C
 N = 100
 A = np.array([[i + j for j in range(N)] for i in range(N)])
@@ -22,4 +23,4 @@ N = 100
 A = np.array([[i + j for j in range(N)] for i in range(N)])
 B = np.array([[i + j for j in range(N)] for i in range(N)])
 A*B
-""", number = 1)) 
+""", number = 1))
