df3475eb6dd4e6a69cab8b3ca30ec624db17939a
Маркіян Мандзак <markmandzak2002@gmail.com>
2022-05-10 11:58:19 +0300

    Task 1.3 : Multiplication Fix Fix

--- Homeworks/1/task.py
+++ Homeworks/1/task.py
@@ -11,7 +11,7 @@ def NaiveMult(A, B, N):
             for k in range(B.shape[0]):
                 C[i][j] += A[i][k] * B[k][j]
     return C
-N = 100
+N = 3
 A = np.array([[i + j for j in range(N)] for i in range(N)])
 B = np.array([[i + j for j in range(N)] for i in range(N)])
 NaiveMult(A, B, N)
@@ -19,8 +19,32 @@ NaiveMult(A, B, N)
 
 print(timeit.timeit("""
 import numpy as np
-N = 100
+N = 3
 A = np.array([[i + j for j in range(N)] for i in range(N)])
 B = np.array([[i + j for j in range(N)] for i in range(N)])
-A*B
+np.dot(A,B)
 """, number = 1))
+
+# import numpy as np
+# def NaiveMult(A, B, N):
+#     m, n = A.shape[0], B.shape[1]
+#     C = [[0 for j in range(m)] for i in range(n)]
+#     for i in range(m):
+#         for j in range(n):
+#             for k in range(B.shape[0]):
+#                 C[i][j] += A[i][k] * B[k][j]
+#     return C
+# N = 3
+# A = np.array([[i + j for j in range(N)] for i in range(N)])
+# B = np.array([[i + j for j in range(N)] for i in range(N)])
+# print(NaiveMult(A, B, N))
+
+# import numpy as np
+# N = 3
+# A = np.array([[i + j for j in range(N)] for i in range(N)])
+# B = np.array([[i + j for j in range(N)] for i in range(N)])
+# print('#'*50)
+# print(A)
+# print(B)
+# print(np.dot(A,B))
+
